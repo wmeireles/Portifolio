2f54a5fb5963710ab9982e485c4093d789151a5f
Willian Meireles <91761339+wmeireles@users.noreply.github.com>
2022-10-06 19:55:43 -0300

    Portifolio

---
+++ js/main.js
@@ -0,0 +1,77 @@
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+    menu.classList.toggle('move');
+};
+
+// Reviews Swiper
+
+var swiper = new Swiper(".reviews-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+// Email JS
+function validate() {
+    let name = document.querySelector('.name')
+    let email = document.querySelector('.email')
+    let msg = document.querySelector('.message')
+    let sendBtn = document.querySelector('.send-btn')
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == "") {
+            emptyerror();
+        } else {
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    })
+
+}
+validate();
+
+function sendmail(name, email, msg) {
+    emailjs.send("service_5xf7xye", "template_ut4rma3", {
+        to_name: name,
+        from_name: email,
+        message: msg,
+    });
+}
+
+function emptyerror() {
+    swal({
+        title: "Oh no....",
+        text: "Fields cannot be empty!",
+        icon: "error",
+    });
+}
+
+function success() {
+    swal({
+        title: "Email sent successfully",
+        text: "We try to reply in 24 hours",
+        icon: "success",
+    });
+}
+
+let header = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-active', window.scrollY > 0);
+});
+
+let scrollTop = document.querySelector('.scroll-top')
+
+window.addEventListener('scroll', () => {
+    scrollTop.classList.toggle('scroll-active', window.scrollY >= 400);
+
+});
